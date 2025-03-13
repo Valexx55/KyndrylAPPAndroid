2994231a473ce1535597a8484e00d51b5a50f7fa
Val <valexx55@gmail.com>
2025-03-13 09:35:32 +0100

    PerrosActivity 3/4

--- app/src/main/java/edu/basic/kyndrylapp/activity/GaleriaPerrosActivity.kt
+++ app/src/main/java/edu/basic/kyndrylapp/activity/GaleriaPerrosActivity.kt
@@ -1,12 +1,19 @@
 package edu.basic.kyndrylapp.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import edu.basic.kyndrylapp.Constantes
 import edu.basic.kyndrylapp.R
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 //aquí nos comunicamos por HTTp y mostaremos los datos
 
 class GaleriaPerrosActivity : AppCompatActivity() {
@@ -19,8 +26,33 @@ class GaleriaPerrosActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val razaPerro =  intent.getStringExtra("RAZA")
+        val razaPerro =  intent.getStringExtra("RAZA") ?: ""
         Toast.makeText(this, "GALERIA $razaPerro", Toast.LENGTH_LONG).show()
+        obtenerListadoRaza(razaPerro)
     }
 
+
+    fun obtenerListadoRaza (raza:String):Unit
+    {
+        var retrofit = Retrofit.Builder() //iniciamos la cofiguración de retrofit
+            .baseUrl("https://dog.ceo/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+        var clientePerroService =  retrofit.create(PerroService::class.java)//creamos el stub/cliente
+
+        //creamos un hilo, para que en un proceso independiente, se produzca la com HTTP
+        //su ámbito depende del de la actividad. Si la activida finaliza, este hilo muere
+        lifecycleScope.launch {
+            var listadoPerros = clientePerroService.listadoDePerrosPorRaza(raza)
+            Log.d(Constantes.ETIQUETA_LOG, "Respuesta obtenida")
+            listadoPerros.message.forEach{ fotoperro ->
+                Log.d(Constantes.ETIQUETA_LOG, "FOTO perro = $fotoperro")
+            }
+        }
+        Log.d(Constantes.ETIQUETA_LOG, "Petición realizada")//UI hilo principal
+
+
+
+    }
 }
--- app/src/main/java/edu/basic/kyndrylapp/activity/PerroService.kt
+++ app/src/main/java/edu/basic/kyndrylapp/activity/PerroService.kt
@@ -2,11 +2,12 @@ package edu.basic.kyndrylapp.activity
 
 import edu.basic.kyndrylapp.ListadoPerros
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 //aquí dentro, declaramos el método que comunican con el api de perros
 interface PerroService {
 
     @GET("api/breed/{raza}/images")
-    suspend fun listadoDePerrosPorRaza (raza:String): ListadoPerros
+    suspend fun listadoDePerrosPorRaza (@Path("raza") raza:String): ListadoPerros
 }
